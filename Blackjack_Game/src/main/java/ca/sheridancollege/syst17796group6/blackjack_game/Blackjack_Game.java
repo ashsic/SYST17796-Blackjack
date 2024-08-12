d91b07eb8701e0713ac7dc524961e9db0691dff5
ashsic <99445200+ashsic@users.noreply.github.com>
2024-08-12 12:04:05 -0400

    adding project

---
+++ Blackjack_Game/src/main/java/ca/sheridancollege/syst17796group6/blackjack_game/Blackjack_Game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.syst17796group6.blackjack_game;
+
+/**
+ *
+ * @author Ashton
+ */
+public class Blackjack_Game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
